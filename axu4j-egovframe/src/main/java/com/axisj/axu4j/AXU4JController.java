43a0aa59cb4f60a080270e78ed14958c9ec4064f
서영아빠 <hyunjun19@gmail.com>
2015-02-12 18:54:34 +0900

    @Controller 추가

--- axu4j-egovframe/src/main/java/com/axisj/axu4j/AXU4JController.java
+++ axu4j-egovframe/src/main/java/com/axisj/axu4j/AXU4JController.java
@@ -1,11 +1,39 @@
 package com.axisj.axu4j;
 
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.EgovFileMngUtil;
+import egovframework.let.cop.bbs.service.EgovBBSAttributeManageService;
+import egovframework.let.cop.bbs.service.EgovBBSManageService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 /**
  * Created by HJ.Park on 2015-02-12.
  */
+@Controller
 public class AXU4JController {
+
+    @Resource(name = "EgovBBSManageService")
+    private EgovBBSManageService bbsMngService;
+
+    @Resource(name = "EgovBBSAttributeManageService")
+    private EgovBBSAttributeManageService bbsAttrbService;
+
+    @Resource(name = "EgovFileMngService")
+    private EgovFileMngService fileMngService;
+
+    @Resource(name = "EgovFileMngUtil")
+    private EgovFileMngUtil fileUtil;
+
+    @Resource(name = "propertiesService")
+    protected EgovPropertyService propertyService;
+
+    @Resource(name="egovMessageSource")
+    EgovMessageSource egovMessageSource;
 
     @RequestMapping("/main.do")
     public void main() { }
