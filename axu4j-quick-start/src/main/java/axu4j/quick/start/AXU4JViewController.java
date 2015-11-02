3fa96500d6832a6db2d4042efa37b035cbe5b1e1
HJ.Park <hyunjun19@gmail.com>
2015-11-02 23:16:47 +0900

    #31 샘플 프로젝트 다시 재정비 시작

--- axu4j-quick-start/src/main/java/axu4j/quick/start/AXU4JViewController.java
+++ axu4j-quick-start/src/main/java/axu4j/quick/start/AXU4JViewController.java
@@ -11,37 +11,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AXU4JViewController {
 
     @RequestMapping("/main")
-    public void main() { }
+    public void main() {
+    }
 
     @RequestMapping("/list")
-    public void list() { }
+    public void list() {
+    }
 
     @RequestMapping("/list-2")
-    public void list2() { }
+    public void list2() {
+    }
 
-    @RequestMapping(value="/help")
-    public void help() { }
+    @RequestMapping(value = "/help")
+    public void help() {
+    }
 
-    @RequestMapping(value="/modal")
-    public void modal() { }
+    @RequestMapping(value = "/modal")
+    public void modal() {
+    }
 
-    @RequestMapping(value={ "/form", "/form-non-axu4j" })
-    public void form() { }
+    @RequestMapping(value = {"/form", "/form-non-axu4j"})
+    public void form() {
+    }
 
-    @RequestMapping(value="/split")
-    public void split() { }
+    @RequestMapping(value = "/split")
+    public void split() {
+    }
 
-    @RequestMapping(value="/table")
-    public void table() { }
+    @RequestMapping(value = "/table")
+    public void table() {
+    }
 
     @RequestMapping("/view")
     public void view() {
-
     }
 
-    @RequestMapping(value={ "/edit", "/edit-non-axu4j" })
-    public void edit(){
-
+    @RequestMapping(value = {"/edit", "/edit-non-axu4j"})
+    public void edit() {
     }
 
 }
